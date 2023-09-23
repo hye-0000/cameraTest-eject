7d562ced34d411ab723f9790c46a87038fca3d36
hye-0000 <hye_0000@naver.com>
2023-09-23 12:17:11 +0900

    Feat: "추가 기본 화면에 카메라 촬영 시작 버튼 추가 후 촬영 시작 되도록"

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import { requestMediaLibraryPermission } from './permissions';
 
 export default function App() {
     const [hasPermission, setHasPermission] = useState(null);
-    const cameraRef = useRef(null);
+    const cameraRef = React.createRef();
     const [isRecording, setIsRecording] = useState(false);
     const [countdown, setCountdown] = useState(5);
+    const [showCamera, setShowCamera] = useState(false); // 카메라 화면 표시 여부
 
     useEffect(() => {
         (async () => {
@@ -18,25 +19,28 @@ export default function App() {
     }, []);
 
     useEffect(() => {
-        // 촬영 시작 버튼을 누른 후 5초 후에 자동으로 촬영 시작
-        let countdownTimer;
-        if (isRecording && countdown > 0) {
-            countdownTimer = setInterval(() => {
-                setCountdown((prevCountdown) => prevCountdown - 1);
-            }, 1000); // 1초마다 countdown 감소
-        } else if (isRecording && countdown === 0) {
-            startRecording();
-        } else {
-            clearInterval(countdownTimer);
-        }
+        if (showCamera) {
+            // 카메라 화면을 표시할 때에만 촬영 시작 타이머를 설정
+            let countdownTimer;
+            if (isRecording && countdown > 0) {
+                countdownTimer = setInterval(() => {
+                    setCountdown((prevCountdown) => prevCountdown - 1);
+                }, 1000); // 1초마다 countdown 감소
+            } else if (isRecording && countdown === 0) {
+                startRecording();
+            } else {
+                clearInterval(countdownTimer);
+            }
 
-        return () => clearInterval(countdownTimer);
-    }, [isRecording, countdown]);
+            return () => clearInterval(countdownTimer);
+        }
+    }, [isRecording, countdown, showCamera]);
 
     // 권한 요청 함수
     const requestCameraPermission = async () => {
         const { status } = await Camera.requestCameraPermissionsAsync();
         setHasPermission(status === 'granted');
+        setShowCamera(true); // 권한 요청 후 카메라 화면 표시
     };
 
     // 촬영 시작 함수
@@ -57,7 +61,7 @@ export default function App() {
         }
     };
 
-    // 동영상 촬영 종료 함수
+    // 촬영 종료 함수
     const stopRecording = async () => {
         if (cameraRef.current) {
             cameraRef.current.stopRecording();
@@ -68,69 +72,54 @@ export default function App() {
 
     // 렌더링
     return (
-        <View style={{ flex: 1 }}>
-            <Camera
-                style={{ flex: 1 }}
-                type={Camera.Constants.Type.back}
-                ref={cameraRef}
-            >
-                <View style={styles.topButtons}>
-                    {!hasPermission && (
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={requestCameraPermission}
-                        >
-                            <Text style={styles.buttonText}>
-                                카메라 권한 요청
-                            </Text>
-                        </TouchableOpacity>
-                    )}
-                    {hasPermission && !isRecording && (
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={() => setIsRecording(true)}
-                        >
-                            <Text style={styles.buttonText}>
-                                촬영 시작
-                            </Text>
-                        </TouchableOpacity>
-                    )}
-                </View>
-                <View style={styles.countdown}>
-                    {countdown > 0 && (
-                        <Text style={styles.countdownText}>{countdown}</Text>
-                    )}
-                </View>
-                {isRecording && (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            {showCamera ? (
+                <Camera
+                    style={{ flex: 1 }}
+                    type={Camera.Constants.Type.back}
+                    ref={cameraRef}
+                >
+                    <View style={styles.countdown}>
+                        {countdown > 0 && (
+                            <Text style={styles.countdownText}>{countdown}</Text>
+                        )}
+                    </View>
+                    <View style={styles.bottomButtons}>
+                        {hasPermission && !isRecording && (
+                            <TouchableOpacity
+                                style={[styles.button, styles.startButton]}
+                                onPress={() => setIsRecording(true)}
+                            >
+                                <Text style={styles.buttonText}>촬영 시작</Text>
+                            </TouchableOpacity>
+                        )}
+                        {isRecording && (
+                            <TouchableOpacity
+                                style={[styles.button, styles.stopButton]}
+                                onPress={stopRecording}
+                            >
+                                <Text style={styles.buttonText}>촬영 종료</Text>
+                            </TouchableOpacity>
+                        )}
+                    </View>
+                </Camera>
+            ) : (
+                <View style={styles.centeredView}>
                     <TouchableOpacity
-                        style={styles.stopButton}
-                        onPress={stopRecording}
+                        style={styles.mainButton}
+                        onPress={requestCameraPermission}
                     >
-                        <Text style={styles.stopButtonText}>촬영 종료</Text>
+                        <Text style={styles.buttonText}>카메라 촬영 시작</Text>
                     </TouchableOpacity>
-                )}
-            </Camera>
+                </View>
+            )}
         </View>
     );
 }
 
+const windowWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-    topButtons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginHorizontal: 16,
-        marginTop: 24,
-    },
-    button: {
-        padding: 12,
-        backgroundColor: 'blue',
-        borderRadius: 8,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 18,
-    },
     countdown: {
         flex: 1,
         justifyContent: 'center',
@@ -140,15 +129,41 @@ const styles = StyleSheet.create({
         fontSize: 48,
         color: 'white',
     },
-    stopButton: {
-        alignSelf: 'center',
-        backgroundColor: 'red',
+    bottomButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 24,
+        marginHorizontal: 16,
+    },
+    button: {
         padding: 12,
         borderRadius: 8,
-        marginTop: 24,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    stopButtonText: {
+    buttonText: {
         color: 'white',
         fontSize: 18,
+        textAlign: 'center',
+    },
+    startButton: {
+        backgroundColor: 'blue',
+    },
+    stopButton: {
+        backgroundColor: 'red',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    mainButton: {
+        padding: 12,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 40, // 원하는 높이로 조절하세요
+        backgroundColor: 'blue',
     },
 });
