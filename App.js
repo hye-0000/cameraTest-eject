23179b29ba64ae34ce12c7f07e64b71a57b11238
hye-0000 <hye_0000@naver.com>
2023-09-24 00:11:09 +0900

    Feat: "추가 동영상 재생(미완)"
    확장자 문제로 추정. 재생이 안됨

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Dimensions, FlatList } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import { requestMediaLibraryPermission } from './permissions';
+import { Video } from 'expo-av';
 
 export default function App() {
     const [hasPermission, setHasPermission] = useState(null);
     const cameraRef = React.createRef();
     const [isRecording, setIsRecording] = useState(false);
     const [countdown, setCountdown] = useState(5);
-    const [showCamera, setShowCamera] = useState(false); // 카메라 화면 표시 여부
-    const [cameraType, setCameraType] = useState(Camera.Constants.Type.back); // 카메라 타입
-
+    const [showCamera, setShowCamera] = useState(false);
+    const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+    const [videoAsset, setVideoAsset] = useState(null);
+    const [selectedVideo, setSelectedVideo] = useState(null);
     useEffect(() => {
         (async () => {
             const { status } = await Camera.requestCameraPermissionsAsync();
@@ -21,12 +23,11 @@ export default function App() {
 
     useEffect(() => {
         if (showCamera) {
-            // 카메라 화면을 표시할 때에만 촬영 시작 타이머를 설정
             let countdownTimer;
             if (isRecording && countdown > 0) {
                 countdownTimer = setInterval(() => {
                     setCountdown((prevCountdown) => prevCountdown - 1);
-                }, 1000); // 1초마다 countdown 감소
+                }, 1000);
             } else if (isRecording && countdown === 0) {
                 startRecording();
             } else {
@@ -37,14 +38,19 @@ export default function App() {
         }
     }, [isRecording, countdown, showCamera]);
 
-    // 권한 요청 함수
+    useEffect(() => {
+        if (selectedVideo) {
+            console.log('설정된 selectedVidoe:', selectedVideo);
+        }
+    }, [selectedVideo]);
+
     const requestCameraPermission = async () => {
         const { status } = await Camera.requestCameraPermissionsAsync();
         setHasPermission(status === 'granted');
-        setShowCamera(true); // 권한 요청 후 카메라 화면 표시
+        setShowCamera(true);
     };
 
-    // 촬영 시작 함수
+
     const startRecording = async () => {
         if (cameraRef.current) {
             try {
@@ -56,22 +62,26 @@ export default function App() {
                 await MediaLibrary.createAlbumAsync('Expo Videos', asset, false);
 
                 console.log('동영상 저장 경로:', uri);
+
+                setVideoAsset(asset);
+                setSelectedVideo(uri);
+                console.log('설정된 URI:', uri); // uri 값을 출력하여 확인
+
             } catch (error) {
                 console.error('동영상 촬영 중 오류:', error);
             }
         }
     };
 
-    // 촬영 종료 함수
     const stopRecording = async () => {
         if (cameraRef.current) {
             cameraRef.current.stopRecording();
-            setIsRecording(false); // 촬영 종료 후 녹화 중 상태를 false로 변경
-            setCountdown(5); // 촬영 종료 후 countdown 초기화
+            setIsRecording(false);
+            setCountdown(5);
+            setShowCamera(false);
         }
     };
 
-    // 카메라 전환 함수
     const toggleCameraType = () => {
         setCameraType(
             cameraType === Camera.Constants.Type.back
@@ -80,13 +90,33 @@ export default function App() {
         );
     };
 
-    // 렌더링
+    const playVideo = () => {
+        console.log('playVideo에서 selectedVideo:', selectedVideo); // selectedVideo 값 확인
+        if (selectedVideo) {
+            // 선택한 비디오 URI가 있다면 해당 URI를 사용하여 동영상을 재생합니다.
+            return (
+                <Video
+                    source={{ uri: selectedVideo }}
+                    rate={1.0}
+                    volume={1.0}
+                    isMuted={false}
+                    shouldPlay={true}
+                    resizeMode="contain"
+                    style={{ flex: 1 }}
+                />
+            );
+        } else {
+            console.log('선택한 동영상이 없습니다.');
+            return null; // 선택한 동영상이 없을 경우, 아무것도 렌더링하지 않습니다.
+        }
+    };
+
     return (
         <View style={{ flex: 1, backgroundColor: 'white' }}>
             {showCamera ? (
                 <Camera
                     style={{ flex: 1 }}
-                    type={cameraType} // 전면 카메라 또는 후면 카메라 선택
+                    type={cameraType}
                     ref={cameraRef}
                 >
                     <View style={styles.topButtons}>
@@ -131,6 +161,12 @@ export default function App() {
                     >
                         <Text style={styles.buttonText}>카메라 촬영 시작</Text>
                     </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.mainButton, styles.playButton]}
+                        onPress={() => playVideo(selectedVideo)}
+                    >
+                        <Text style={styles.buttonText}>재생</Text>
+                    </TouchableOpacity>
                 </View>
             )}
         </View>
@@ -142,9 +178,9 @@ const windowWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
     topButtons: {
         flexDirection: 'row',
-        justifyContent: 'flex-start', // 왼쪽으로 정렬
+        justifyContent: 'flex-start',
         marginHorizontal: 20,
-        marginTop: 20, // 버튼과 상단 간격 설정
+        marginTop: 20,
     },
     countdown: {
         flex: 1,
@@ -192,7 +228,7 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         alignItems: 'center',
         justifyContent: 'center',
-        height: 40, // 원하는 높이로 조절하세요
+        height: 40,
         backgroundColor: 'blue',
     },
 });
